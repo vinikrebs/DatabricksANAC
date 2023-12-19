7ae80f3ab24029a3698a0a12c83cdeb6812f3278
vinikrebs <vinikrebs>
2023-12-19 17:38:57 +0000

    Inclusão das referências

---
+++ 1. Referências Databricks Azure.py
@@ -0,0 +1,25 @@
+# Databricks notebook source
+Referecia Databicks Azure
+o que é lakehouse: https://learn.microsoft.com/pt-br/azure/databricks/lakehouse/
+
+O que é arquitetura medallion do Lakehouse? https://learn.microsoft.com/pt-br/azure/databricks/lakehouse/medallion
+
+Pipelines de processamento de ingestão, ETL e fluxo com o Azure Databricks e o Delta Lake https://learn.microsoft.com/pt-br/azure/architecture/solution-ideas/articles/ingest-etl-stream-with-adb
+
+Grupos de recursos do Azure https://learn.microsoft.com/pt-br/azure/azure-resource-manager/management/manage-resource-groups-portal
+
+Introdução ao Azure Data Lake Storage Gen2 https://learn.microsoft.com/pt-br/azure/storage/blobs/data-lake-storage-introduction
+
+Autenticação e autorização no Serviço https://learn.microsoft.com/pt-br/azure/app-service/overview-authentication-authorization
+
+O que é a gestão de identidades e acessos (IAM)? https://learn.microsoft.com/pt-br/azure/active-directory/fundamentals/introduction-identity-access-management
+
+ACLs (Listas de controle de acesso) no Azure Data Lake Storage Gen2 https://learn.microsoft.com/pt-br/azure/storage/blobs/data-lake-storage-access-control
+
+Gerenciador de Armazenamento do Microsoft Azure https://azure.microsoft.com/pt-br/products/storage/storage-explorer
+
+Runtimes do Azure Synapse https://learn.microsoft.com/pt-br/azure/synapse-analytics/spark/apache-spark-version-support
+
+Runtime do Synapse para ciclo de vida e suporte do Apache Spark https://learn.microsoft.com/pt-br/azure/synapse-analytics/spark/runtime-for-apache-spark-lifecycle-and-supportability
+
+Valor Cluster https://azure.microsoft.com/en-gb/pricing/details/databricks/
